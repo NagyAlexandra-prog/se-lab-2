42c3a5f3946d7463f98da0b7e1452183aa14d59d
unknown <176192@BME.HU>
2023-10-25 15:47:08 +0200

    Done

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,4 +18,26 @@ class TorpedoStoreTest {
     // Assert
     assertEquals(true, result);
   }
+
+  @Test
+  void fire_Fail(){
+    // Arrange
+    TorpedoStore store = new TorpedoStore(1,1);
+
+    // Act
+    boolean result = store.fire(1);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  void fire_TooMuch(){
+    // Arrange
+    TorpedoStore store = new TorpedoStore(1);
+
+    // Assert
+    assertThrows(IllegalArgumentException.class, () -> store.fire(2));
+  }
+
 }
